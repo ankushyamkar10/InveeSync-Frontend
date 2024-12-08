98b1f0e029d107a40734026f104872458cc43c31
Ankush  Baburao Yamkar <100845752+ankushyamkar10@users.noreply.github.com>
2024-12-08 19:57:04 +0530

    keeps abc as string 123 as Numeric

--- src/app/validations/BoMValidation.js
+++ src/app/validations/BoMValidation.js
@@ -48,11 +48,11 @@ export const validateSingleBoM = (row, uniqueCombinations, uniqueIds, itemsTypes
   }
 
 
-  if (typeof quantity !== 'number') {
+  if (isNaN(quantity) || quantity === '' || quantity === null) {
     errors.push("Quantity must be a valid number");
   } else {
     const quantityVal = parseFloat(quantity);
-    if ( isNaN(quantity) ||
+    if (
       quantityVal < BOM_CONSTANTS.MIN_QUANTITY ||
       quantityVal > BOM_CONSTANTS.MAX_QUANTITY
     ) {
